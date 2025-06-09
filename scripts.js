8cb8ffc25794d859c3fd837e8912273e23b7a7b5
Lucas5963 <lucasaraujo6878@gmail.com>
2025-06-08 23:29:43 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+const modal = document.querySelector(".modal")
+const mascara = document.querySelector(".mascara-modal")
+
+function mostarModal() {
+    modal.style.left = "50%"
+    mascara.style.visibility = "visible"
+}
+function esconderModal() {
+    modal.style.left = "-30%"
+    mascara.style.visibility = "hidden"
+}
